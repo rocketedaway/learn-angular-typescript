9ab1f45120cae782705ab8f3ab7fe4bc013b6fb7
rocketedaway <randy.wilander@gmail.com>
2017-08-29 17:20:34 -0700

    Finished step 6. Added order filter via two-way binding.

--- src/components/phone-list/phone-list-controller.ts
+++ src/components/phone-list/phone-list-controller.ts
@@ -2,17 +2,23 @@
 
 export default class PhoneListController {
   public phones: Array<Object>
+  public query: String
+  public order: String
 
   constructor () {
+    this.order = "age";
     this.phones = [
       {
         name: 'Nexus S',
+        age: 1,
         snippet: 'Fast just got faster with Nexus S.'
       }, {
         name: 'Motorola XOOM™ with Wi-Fi',
+        age: 3,
         snippet: 'The Next, Next Generation tablet.'
       }, {
         name: 'MOTOROLA XOOM™',
+        age: 2,
         snippet: 'The Next, Next Generation tablet.'
       }
     ];
